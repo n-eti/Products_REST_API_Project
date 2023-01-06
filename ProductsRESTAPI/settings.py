75af9fa539f80b1d1895812b11dd8a5ce20fac02
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-06 14:20:24 -0600

    Created a localsettings file in gitignore, updated databases in settings.

--- ProductsRESTAPI/settings.py
+++ ProductsRESTAPI/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*xuubuo@r8lro-hvbwk7clx&-d5e*5cy(0_%%)+xq#at4)l^%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,13 +71,6 @@ WSGI_APPLICATION = 'ProductsRESTAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -121,3 +112,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+try:
+    from ProductsRESTAPI.local_settings import *
+except ImportError:
+    pass
