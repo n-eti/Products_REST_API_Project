558ae6655d2b292c7d2fa1fc8a7ae1ae3ec51798
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-09 17:00:07 -0600

    Created data with POST requests

--- products/views.py
+++ products/views.py
@@ -3,10 +3,18 @@ from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    products = Product.objects.all()
 
-    serializer = ProductSerializer(products, many=True)
+    if request.method == 'GET':    
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
 
-    return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid() == True:
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.errors, status=400)
