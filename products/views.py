4ac726a89cb8992047845a9e075a369ab6019d09
Nada Atieh <nada.s.atieh@gmail.com>
2023-01-10 08:09:47 -0600

    Registered products model with admin site + completed post functionality for Product

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def products_list(request):
@@ -13,8 +14,6 @@ def products_list(request):
 
     elif request.method == 'POST':
         serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid() == True:
-            serializer.save()
-            return Response(serializer.data, status=201)
-        else:
-            return Response(serializer.errors, status=400)
+        serializer.is_valid(raise_exception=True)  
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
